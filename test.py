26c9451546fd02f199d742c996fadd4bbf7a23eb
Tyler Huntington <thuntin1@swarthmore.edu>
2018-01-11 09:03:57 -0800

    Add new tests for access to task list page

--- test.py
+++ test.py
@@ -50,18 +50,18 @@ class AllTests(unittest.TestCase):
 
     # test user creation
     def test_user_setup(self):
-        new_user = User("michael", "michael@herman.com", "michaelherman")
+        new_user = User("tylertarr", "tylertarr@huntington.com", "tylertarrhuntington")
         db.session.add(new_user)
         db.session.commit()
         test = db.session.query(User).all()
         for t in test:
             t.name
-        assert(t.name == 'michael')
+        assert(t.name == 'tylertarr')
     
     # test that unregistered users can't log in
     def test_invalid_form_data(self):
-        self.register("michael", "michael@herman.com", "michaelherman", "michaelherman")
-        response = self.login("wrongusername", "michaelherman")
+        self.register("tylertarr", "tylertarr@huntington.com", "tylertarrhuntington", "tylertarrhuntington")
+        response = self.login("wrongusername", "tylertarrhuntington")
         self.assertIn(b'Invalid username or password', response.data)
 
     # test that form is present on registration page
@@ -75,31 +75,31 @@ class AllTests(unittest.TestCase):
 
         # first check that registration works
         self.app.get("register/", follow_redirects=True)
-        response = self.register(name="michael",
-                email="michael@herman.com",
-                password="michaelherman",
-                confirm="michaelherman")
+        response = self.register(name="tylertarr",
+                email="tylertarr@huntington.com",
+                password="tylertarrhuntington",
+                confirm="tylertarrhuntington")
         self.assertIn(b'Thanks for registering, please log in.', response.data)
         
         # now check that login works
-        response = self.login("michael", "michaelherman")
+        response = self.login("tylertarr", "tylertarrhuntington")
         self.assertIn(b"Welcome", response.data)
 
     # test that user registration error thrown when duplicate username used
     def test_user_registration_error(self):
         self.app.get('register/', follow_redirects=True)
-        self.register(name='michael', email='michael@herman.com', 
-                password='michaelherman', confirm='michaelherman')
+        self.register(name='tylertarr', email='tylertarr@huntington.com', 
+                password='tylertarrhuntington', confirm='tylertarrhuntington')
         self.app.get('register/', follow_redirects=True)
-        response = self.register(name='michael', email='michael@herman.com', 
-                password='michaelherman', confirm='michaelherman')
+        response = self.register(name='tylertarr', email='tylertarr@huntington.com', 
+                password='tylertarrhuntington', confirm='tylertarrhuntington')
         self.assertIn(b'That username and/or email already exist', response.data)
 
     # test that logged in users can log out
     def test_logged_in_users_can_log_out(self):
-        self.register("michael", "michael@herman.com", 
-                "michaelherman", "michaelherman")
-        self.login(name="michael", password="michaelherman")
+        self.register("tylertarr", "tylertarr@huntington.com", 
+                "tylertarrhuntington", "tylertarrhuntington")
+        self.login(name="tylertarr", password="tylertarrhuntington")
         response = self.logout()
         self.assertIn(b"Successfully logged out", response.data)
 
@@ -107,6 +107,21 @@ class AllTests(unittest.TestCase):
     def test_not_logged_in_users_cannot_logout(self):
         response = self.logout()
         self.assertNotIn(b"Successfully logged out", response.data)
+
+    # test that logged in users can view tasks
+    def test_logged_in_users_can_view_tasks(self):
+        self.register("tylertarr", "tyler@tarr.com",
+                "tylerhuntington", "tylerhuntington")
+        response = self.login("tylertarr", "tylerhuntington")
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"Add a new task:", response.data)
+
+    # test that not logged in users cannot access tasks page
+    def test_not_logged_in_users_cannot_access_tasks_page(self):
+        response = self.app.get('tasks/', follow_redirects=True)
+        self.assertIn(b"You need to log in first", response.data)
+
+
         
 
 
