4384d875f5131ba0340c72597c75e9539c94adef
Tyler Huntington <thuntin1@swarthmore.edu>
2018-01-17 19:14:13 -0800

    Create Procfile and set up for Heroku depoloyment

--- run.py
+++ run.py
@@ -5,6 +5,8 @@ Wrapper script for launching Docket app.
 
 Tyler Huntington, 2018
 """
-
+import os
 from project import app
-app.run()
+
+port = int(os.environ.get('PORT', 5000))
+app.run(host='0.0.0.0', port=port)
